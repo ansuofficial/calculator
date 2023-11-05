f0f67d6cc69a6e754882b1b7750337e6a9fe9e80
Ansuofficial <badjieansu165@gmail.com>
2023-11-05 10:11:05 +0000

    First commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import {App} from './app.jsx'
+import './app.css'
+
+ReactDom.createRoot(document.getElementById('root')).render(<App/>)
